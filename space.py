529f2c5f33928dccb13c674bd12ef8340b8ae746
MalCaor <xav.lemen@orange.fr>
2021-03-09 13:51:20 +0100

    comment

--- space.py
+++ space.py
@@ -6,29 +6,38 @@ import tkinter as tk
 class player:
     # Player Class
     def __init__(self, id, canvas, x, y):
+        # player id
         self.id = id
+        # player pos
         self.x = x
         self.y = y
+        # player movement
         self.mX = 0
         self.mY = 0
+        # player lives
         self.life = 3
+        # player sprite
         self.img = ImageTk.PhotoImage(Image.open("img/main_char.png"))
         self.imgv2 = self.img._PhotoImage__photo.zoom(2)
         self.canvas = canvas
         self.sprite = self.canvas.create_image(x, y, image=self.imgv2)
+        # console message when init finish
         print("player initialized")
 
     def right(self, event):
+        # function to move right
         print(event.keysym)
         self.mX = 5
         self.mY = 0
 
     def left(self, event):
+        # function to move left
         print(event.keysym)
         self.mX = -5
         self.mY = 0
 
     def movement(self):
+        # fuction called to move
         print("move to x:" + str(self.x) + " y:" + str(self.y))
         self.canvas.move(self.sprite, self.mX, self.mY)
         self.canvas.after(100, self.movement)
@@ -39,6 +48,7 @@ class player:
 
 class Space:
     def __init__(self):
+        # init canvas
         self.root = tk.Tk()
         self.canvas_width = 600
         self.canvas_height = 400
@@ -55,9 +65,12 @@ class Space:
         self.listP = [p]
     
     def start(self):
+        # fuction on loop
         self.listP[0].movement()
+        # key binding
         self.root.bind("<KeyPress-Left>", lambda e: self.listP[0].left(e))
         self.root.bind("<KeyPress-Right>", lambda e: self.listP[0].right(e))
+        # etc...
         self.root.mainloop()
         
 ex = Space()
